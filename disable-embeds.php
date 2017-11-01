77b63bd3df2c9ac43af999c51a8d1460dfa1b5cf
Jesse Nickles <jesse@littlebizzy.com>
2017-11-01 15:19:02 -0700

    Update disable-embeds.php

--- disable-embeds.php
+++ disable-embeds.php
@@ -3,7 +3,7 @@
 Plugin Name: Disable Embeds
 Plugin URI: https://wordpress.org/plugins/disable-embeds-littlebizzy/
 Description: Disables both external and internal embedding functions to avoid slow page render, instability and SEO issues, and to improve overall loading speed.
-Version: 1.0.0
+Version: 1.0.1
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -11,14 +11,18 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Prefix: DSBEBD
 */
 
-// Avoid script calls via plugin URL
+// Admin Notices module
+require_once dirname(__FILE__).'/admin-notices.php';
+DSBEBD_Admin_Notices::instance(__FILE__);
+
+// Block direct calls
 if (!function_exists('add_action'))
 	die;
 
 // This plugin constants
 define('DSBEBD_FILE', __FILE__);
 define('DSBEBD_PATH', dirname(DSBEBD_FILE));
-define('DSBEBD_VERSION', '1.0.0');
+define('DSBEBD_VERSION', '1.0.1');
 
 // Load main class
 require_once DSBEBD_PATH.'/core/core.php';
