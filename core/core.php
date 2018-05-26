37fcd6dee752b57726accc29eb977aa8633a74c7
Pau <pauiglesias@gmail.com>
2018-05-26 19:25:21 +0200

    Fixed bad static function declaration.

--- core/core.php
+++ core/core.php
@@ -56,7 +56,7 @@ final class Core extends Helpers\Singleton {
 	/**
 	 * Plugin deactivation
 	 */
-	public static function deactivation() {
+	public function deactivation() {
 		add_filter('rewrite_rules_array', [$this->plugin->factory->cleaner, 'rules']);
 		flush_rewrite_rules();
 	}
