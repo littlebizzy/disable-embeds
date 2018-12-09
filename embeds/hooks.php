242529b56ef2e6b1fc71b21fc4c55ae703816e68
Pau <pauiglesias@gmail.com>
2018-12-09 22:02:30 +0100

    Removed pass by reference in callable arrays.

--- embeds/hooks.php
+++ embeds/hooks.php
@@ -46,7 +46,7 @@ class Hooks {
 		$this->plugin = $plugin;
 
 		// Init hook
-		add_action('init', [&$this, 'init'], PHP_INT_MAX);
+		add_action('init', [$this, 'init'], PHP_INT_MAX);
 	}
 
 
@@ -72,8 +72,8 @@ class Hooks {
 			$this->plugin->factory->cleaner->oembedCache();
 
 			// Process detected exceptions
-			add_filter('oembed_providers',  [&$this, 'providers']);
-			add_filter('pre_oembed_result', [&$this, 'preResults'], 10, 2);
+			add_filter('oembed_providers',  [$this, 'providers']);
+			add_filter('pre_oembed_result', [$this, 'preResults'], 10, 2);
 
 		// No exceptions
 		} else {
