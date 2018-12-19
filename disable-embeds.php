7592d759929cd105d92768a07fb30bc6af079523
Pau <pauiglesias@gmail.com>
2018-12-19 18:30:08 +0100

    Updated main plugin file removing old initialization code.

--- disable-embeds.php
+++ disable-embeds.php
@@ -8,35 +8,20 @@ Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
+PBP Version: 1.1.0
+WC requires at least: 3.3
+WC tested up to: 3.5
 Prefix: DSBEBD
 */
 
 // Plugin namespace
 namespace LittleBizzy\DisableEmbeds;
 
-// Block direct calls
-if (!function_exists('add_action'))
-	die;
-
 // Plugin constants
 const FILE = __FILE__;
 const PREFIX = 'dsbebd';
 const VERSION = '1.3.0';
 
-// Loader
-require_once dirname(FILE).'/helpers/loader.php';
-
-// Admin Notices
-Admin_Notices::instance(__FILE__);
-
-/**
- * Admin Notices Multisite check
- * Uncomment "//return" to disable this plugin on Multisite installs
- */
-require_once dirname(__FILE__).'/admin-notices-ms.php';
-if (false !== Admin_Notices_MS::instance(__FILE__)) {
-	//return;
-}
-
-// Run the main class
-Helpers\Runner::start('Core\Core', 'instance');
+// Boot
+require_once dirname(FILE).'/helpers/boot.php';
+Helpers\Boot::instance(FILE);
